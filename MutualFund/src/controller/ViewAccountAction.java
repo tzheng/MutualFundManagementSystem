4bf774b5584112cce34557b4bd4f568320291dca
Shoucong Chen <Shoucong Chen@ShoucongChen>
2013-01-20 13:26:42 -0500

    Add ViewAccountAction, update customer-viewaccount.jsp

--- MutualFund/src/controller/ViewAccountAction.java
+++ MutualFund/src/controller/ViewAccountAction.java
@@ -23,7 +23,7 @@ public class ViewAccountAction extends Action {
 		customerDAO = model.getCustomerDAO();
 	}
 
-	public String getName() { return "manage.do"; }
+	public String getName() { return "view-account.do"; }
 
 	public String perform(HttpServletRequest request) {
         // Set up the errors list
@@ -33,9 +33,10 @@ public class ViewAccountAction extends Action {
 		try {
 			int customerId = (Integer) request.getSession(false).getAttribute("customerId");
 			CustomerBean customer = customerDAO.read(customerId);
-	        request.setAttribute("favoriteList",favoriteList);
+			
+			request.setAttribute("customer", customer);
 
-	        return "manage.jsp";
+	        return "customer-viewaccount.jsp";
         } catch (MyDAOException e) {
         	errors.add(e.getMessage());
         	return "error.jsp";
--- MutualFund/src/model/CustomerDAO.java
+++ MutualFund/src/model/CustomerDAO.java
@@ -5,8 +5,13 @@
 
 package model;
 
-import java.sql.*;
-import java.util.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import model.MyDAOException;
 
 import databean.CustomerBean;
 
