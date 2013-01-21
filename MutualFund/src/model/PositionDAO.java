080364d1bbcc2cdf7739a793371eb30884cdb4b5
Shoucong Chen <Shoucong Chen@128.237.166.170>
2013-01-20 19:27:15 -0500

    Add PositionBean, update PositionDAO and ViewAccountAction

--- MutualFund/src/model/PositionDAO.java
+++ MutualFund/src/model/PositionDAO.java
@@ -1,12 +1,58 @@
 package model;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import databean.PositionBean;
 
 public class PositionDAO extends BaseDAO {
 	public PositionDAO(String jdbcDriver, String jdbcURL, String tableName) throws MyDAOException {
 		super(jdbcDriver, jdbcURL, tableName);
+	}
+	
+	public PositionBean[] getUserPositions(int customerId) throws MyDAOException {
+		Connection con = null;
+    	try {
+        	con = getConnection();
+        	con.setAutoCommit(false);
+        	
+        	PreparedStatement pstmt = con.prepareStatement(
+        			"SELECT * FROM " + tableName + " WHERE customerId = ?");
+        	pstmt.setInt(1, customerId);
+
+            ResultSet rs = pstmt.executeQuery();
+            
+            List<PositionBean> list = new ArrayList<PositionBean>();
+            while (rs.next()) {
+            	PositionBean bean = new PositionBean();
+            	bean.setCustomerId(rs.getInt("customerId"));
+            	bean.setFundId(rs.getInt("fundId"));
+            	bean.setShares(rs.getInt("shares"));
+            	list.add(bean);
+            }
+            rs.close();
+            pstmt.close();
+            
+            con.commit();
+            con.setAutoCommit(true);
+            releaseConnection(con);
+            
+            return list.toArray(new PositionBean[list.size()]);
+            
+    	} catch (SQLException e) {
+            try {
+            	if (con != null) {
+            		con.rollback();
+            		con.close();
+            	}
+            } catch (SQLException e2) { /* ignore */ }
+        	throw new MyDAOException(e);
+		}
 	}
 
 	@Override
@@ -27,5 +73,4 @@ public class PositionDAO extends BaseDAO {
         	throw new MyDAOException(e);
         }
 	}
-
 }
