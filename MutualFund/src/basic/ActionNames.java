219eb65af7cf4173caf901784b2e388f4d7b8f26
chinesecold <chinesecold@gmail.com>
2013-01-29 13:59:00 -0500

    Controller modified

--- MutualFund/src/basic/ActionNames.java
+++ MutualFund/src/basic/ActionNames.java
@@ -1,6 +1,6 @@
 package basic;
 
 public interface ActionNames {
-	public static final String[] customerActions = new String[]{"customer-requestcheck.do", "customer-buyfund.do", "customer-change-pwd.do", "customer-sellfund.do", "customerhistory.do"};
-	public static final String[] employeeActions = new String[]{"create-account.do", "create-employee-account.do", "createfund.do", "employee-depositcheck.do", "employee-change-pwd.do", "employee-reset-customer-pwd.do", "employeehistory.do"};
+	public static final String[] customerActions = new String[]{"view-account.do", "customer-requestcheck.do", "customer-buyfund.do", "customer-change-pwd.do", "customer-sellfund.do", "customerhistory.do", "customer-viewfundpricehistory.do", "customer-mainpanel.do", "customer-research-fund.do", "customerhistory.do"};
+	public static final String[] employeeActions = new String[]{"create-account.do", "create-employee-account.do", "createfund.do", "employee-depositcheck.do", "employee-change-pwd.do", "employee-reset-customer-pwd.do", "employeehistory.do", "employee-transitionday.do", "employeeviewaccount.do", "employeehistory.do"};
 }
