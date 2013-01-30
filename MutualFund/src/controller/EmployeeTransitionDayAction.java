fee868f5d797f0ace7bc1cda05f57a52a462423d
tzheng <tzheng@andrew.cmu.edu>
2013-01-30 11:50:02 -0500

    Calculation BUG

--- MutualFund/src/controller/EmployeeTransitionDayAction.java
+++ MutualFund/src/controller/EmployeeTransitionDayAction.java
@@ -337,7 +337,7 @@ public class EmployeeTransitionDayAction extends Action {
 	public double divide(double v1, double v2, int scale) {
 		BigDecimal b1 = new BigDecimal(Double.toString(v1));
 		BigDecimal b2 = new BigDecimal(Double.toString(v2));
-		return b1.divide(b2, scale, BigDecimal.ROUND_HALF_UP).doubleValue();
+		return b1.divide(b2, scale, BigDecimal.ROUND_DOWN).doubleValue();
 	}
 	
 	public Date getTomorrow(Date today) {
